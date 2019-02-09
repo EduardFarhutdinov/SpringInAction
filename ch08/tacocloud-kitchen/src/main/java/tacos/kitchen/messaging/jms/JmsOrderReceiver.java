dddf8a5c2b794d9189700418fc5eb1f2c5769704
EduardFarhutdinov <farhutdinoveduard16@gmail.com>
2019-02-09 18:55:04 +0300

    chapter-08-commit-02

--- ch08/tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/jms/JmsOrderReceiver.java
+++ ch08/tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/jms/JmsOrderReceiver.java
@@ -11,15 +11,15 @@ import tacos.kitchen.OrderReceiver;
 @Component("templateOrderReceiver")
 public class JmsOrderReceiver implements OrderReceiver {
 
-  private JmsTemplate jms;
+    private JmsTemplate jms;
 
-  public JmsOrderReceiver(JmsTemplate jms) {
-    this.jms = jms;
-  }
-  
-  @Override
-  public Order receiveOrder() {
-    return (Order) jms.receiveAndConvert("tacocloud.order.queue");
-  }
-  
+    public JmsOrderReceiver(JmsTemplate jms)     {
+        this.jms = jms;
+    }
+
+    @Override
+    public Order receiveOrder() {
+        return (Order) jms.receiveAndConvert("tacocloud.order.queue");
+    }
+
 }
--- ch08/tacocloud-messaging-jms/src/main/java/tacos/messaging/JmsOrderMessagingService.java
+++ ch08/tacocloud-messaging-jms/src/main/java/tacos/messaging/JmsOrderMessagingService.java
@@ -8,8 +8,10 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import tacos.Order;
+import tacos.Taco;
+import tacos.User;
 
-@Service
+  @Service
 public class JmsOrderMessagingService implements OrderMessagingService {
 
   private JmsTemplate jms;
@@ -24,10 +26,13 @@ public class JmsOrderMessagingService implements OrderMessagingService {
     jms.convertAndSend("tacocloud.order.queue", order,
         this::addOrderSource);
   }
-  
+
+
   private Message addOrderSource(Message message) throws JMSException {
     message.setStringProperty("X_ORDER_SOURCE", "WEB");
     return message;
   }
+
+
 
 }
